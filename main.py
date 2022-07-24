b307d73256351e783d7c48c16baf2573d4c3989d
Nidjo <af7er.g@gmail.com>
2022-07-24 23:13:54 +0200

    created Python function and program loop which appends user input (word for given reactions).
    Continued with secret number code and started with database config!

--- main.py
+++ main.py
@@ -1,18 +1,70 @@
-from flask import Flask, render_template
+import random
+
+#importi potrebni za response i cookies:
+from flask import Flask, render_template, request, make_response
+
+#importi potrebni za db:
+from flask import redirect, url_for
+from sqla_wrapper import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    secret_number = request.cookies.get("secret_number")  # check if there is already a cookie named secret_number
 
-@app.route("/about")
-def aa():
-    return render_template("about.html")
+    response = make_response(render_template("index.html"))
+    if not secret_number:  # if not, create a new cookie
+        new_secret = random.randint(1, 30)
+        response.set_cookie("secret_number", str(new_secret))
 
-@app.route("/questions")
-def bb():
-    return render_template("questions.html")
+    return response
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    guess = int(request.form.get("guess"))
+    secret_number = int(request.cookies.get("secret_number"))
+
+    if guess == secret_number:
+        message = "Correct! The secret number is {0}".format(str(secret_number))
+        response = make_response(render_template("result.html", message=message))
+        response.set_cookie("secret_number", str(random.randint(1, 30)))  # set the new secret number
+        return response
+    elif guess > secret_number:
+        message = "Your guess is not correct... try something smaller."
+        return render_template("result.html", message=message)
+    elif guess < secret_number:
+        message = "Your guess is not correct... try something bigger."
+        return render_template("result.html", message=message)
+
+@app.route("/simple_db")
+def db():
+    return render_template("simple_db.html")
+
+@app.route("/add-message", methods=["POST"])
+def add_message():
+    username = request.form.get("username")
+    text = request.form.get("text")
+
+    print("{0}: {1}".format(username, text))
+
+    return redirect("/")
+
+
+#ovaj dio koda se dotice db.
+
+db = SQLAlchemy("sqlite:///db.sqlite")
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String, unique=False)
+    text = db.Column(db.String, unique=False)
+
+
+db.create_all()
 
 if __name__ == '__main__':
-    app.run(use_reloader=True)
+    app.run(use_reloader=True)  # if you use the port parameter, delete it before deploying to Heroku
